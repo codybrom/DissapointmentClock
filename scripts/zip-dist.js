764ef945e9ac206c8955e8daa4b16221ceabc2b8
Cody Bromley <codybrom@users.noreply.github.com>
2023-11-03 10:34:13 -0500

    add zip build script

---
+++ scripts/zip-dist.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const archiver = require('archiver');
+const {name} = require('../package.json'); // Update the path to package.json
+
+const timestamp = Math.floor(Date.now() / 1000); // Get current time in Unix format
+
+const outputFolder = 'out'; // Name of the output folder
+const outputFilePath = `${outputFolder}/${name}-${timestamp}.zip`; // Path to the output zip file
+
+// Create the output folder if it does not exist
+if (!fs.existsSync(outputFolder)) {
+  fs.mkdirSync(outputFolder);
+}
+
+const output = fs.createWriteStream(outputFilePath);
+const archive = archiver('zip', {zlib: {level: 9}});
+
+output.on('close', () => {
+  console.log(`Successfully zipped the dist folder. Output file: ${outputFilePath}`);
+});
+
+archive.on('error', err => {
+  throw err;
+});
+
+archive.pipe(output);
+archive.directory('../dist/', false); // Update the path to the dist folder
+archive.finalize();
